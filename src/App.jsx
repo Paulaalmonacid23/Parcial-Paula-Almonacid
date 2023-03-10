5b364f2fafcf00570838e06725dd7ed5f014cbe0
paula <paulaalmonacid23@gmail.com>
2023-03-10 16:41:46 -0500

    parcial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Formulario from './formulario';
+
+const App = () => {
+return (
+    <div> 
+        <Formulario/>
+        <h2>hola</h2>
+    </div>
+)
+}
+export default App
+
+
---
+++ src/Card.jsx
@@ -0,0 +1,9 @@
+function Card(props) {
+    return (
+    <div>
+        <h2> Esta es un componente
+    </h2>
+    </div>
+    );
+}
+export default Card;
